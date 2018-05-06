5f1ae9bb00845c2c25c6147026c7f874e372be0a
Tomas Gonzalez C <tomas.83.gonzalez@gmail.com>
2018-05-06 08:59:16 -0300

    se suben cambios al proyecto rama develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ProveedoresService } from 'app/servicios/proveedores.service';
 
 import { AppComponent } from './app.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
